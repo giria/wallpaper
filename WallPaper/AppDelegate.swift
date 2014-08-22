9d976444da3f7912fad9633385692f973517ddff
giria <joan.barrull@gmail.com>
2014-08-22 20:35:37 +0200

    first commit

--- WallPaper/AppDelegate.swift
+++ WallPaper/AppDelegate.swift
@@ -5,8 +5,9 @@
 //  Created by Joan Barrull Ribalta on 22/08/14.
 //  Copyright (c) 2014 com.giria. All rights reserved.
 //
-
+import Foundation
 import Cocoa
+import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
@@ -15,6 +16,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
+        var imgURL: NSURL? = NSURL.fileURLWithPath("/Users/joanbarrullribalta/homerWallpaper.jpeg")
+        var error: NSError?
+        var workspace = NSWorkspace.sharedWorkspace()
+        var screen = NSScreen.mainScreen()
+        
+        var result : Bool = workspace.setDesktopImageURL(imgURL, forScreen: screen, options: nil, error:&error)
+        if result {
+            println("Wallppaper set")
+        }
+
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
