d498bc9aa147af9b212f18abe3bd4082af19f797
giria <joan.barrull@gmail.com>
2014-08-22 19:56:51 +0200

    Initial Commit

---
+++ WallPaper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WallPaper
+//
+//  Created by Joan Barrull Ribalta on 22/08/14.
+//  Copyright (c) 2014 com.giria. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WallPaper/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  WallPaper
+//
+//  Created by Joan Barrull Ribalta on 22/08/14.
+//  Copyright (c) 2014 com.giria. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
